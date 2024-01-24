8db33e2d4a29341bc8e87dc7c41cdbd0d3267508
bimpe <adeyinminuadebimpe@gmail.com>
2024-01-24 00:52:51 +0000

    updated db.js

--- db.js
+++ db.js
@@ -7,20 +7,6 @@ let connection;
 
 const uri = process.env.DB_URL;
 
-const connectToDb = async () => {
-  try {
-    connection = await MongoClient.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverApi: ServerApiVersion.v1,
-    });
-    console.log("Connected to MongoDB Atlas");
-  } catch (err) {
-    console.log("Error connecting to MongoDB Atlas: ", err);
-    throw err;
-  }
-};
-
 const getDb = () => {
   if (!connection) {
     throw new Error("Call connectToDb() before calling getDb()");
