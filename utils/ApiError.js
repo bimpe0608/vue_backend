c55cd44b176fd400e4656057d6b3f414cf9b5b1a
bimpe <adeyinminuadebimpe@gmail.com>
2024-01-28 20:12:06 +0000

    created error class

---
+++ utils/ApiError.js
@@ -0,0 +1,15 @@
+//creating a custom error class for my server so I can format javascript error contructor to json
+class ApiError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = ApiError;
